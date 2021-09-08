a01b465ff1172624d5fd3e15fe6e9e52468e4649
Viktor Ferenczi <ferencziviktor11@gmail.com>
2021-09-08 14:27:43 +0200

    up and down direction added

--- SwipeGestureHandler.js
+++ SwipeGestureHandler.js
@@ -8,15 +8,23 @@ import {
 const { width, height } = Dimensions.get("window");
 
 const getDirection = ({ moveX, moveY, dx, dy }) => {
+  const draggedDown = dy > 30;
+  const draggedUp = dy < -30;
   const draggedLeft = dx < -30;
   const draggedRight = dx > 30;
+
   let dragDirection = "";
+
+  if (draggedDown || draggedUp) {
+    if (draggedDown) dragDirection += "down";
+    if (draggedUp) dragDirection += "up";
+  }
 
   if (draggedLeft || draggedRight) {
     if (draggedLeft) dragDirection += "left";
-    if (draggedRight) dragDirection += "right";
+    if (draggedRight) dragDirection += "ight";
   }
-
+  
   if (dragDirection) return dragDirection;
 };
 
@@ -25,10 +33,10 @@ export default class SwipeGesture extends Component {
   componentWillMount = () => {
     this._panResponder = PanResponder.create({
       onMoveShouldSetPanResponder: (evt, gestureState) => !!getDirection(gestureState),
-      onPanResponderTerminationRequest: (evt, gestureState) => true,
       onPanResponderRelease: (evt, gestureState) => {
         this.props.onSwipe(getDirection(gestureState));
       },
+      onPanResponderTerminationRequest: (evt, gestureState) => true,
     });
   }
 
